ac10dbf9973993aa2d51ed0c6e093ae2a116c516
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2023-11-22 19:34:27 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print ("this is a simple Calculator")
+
+x = int (input("x: "))
+y = int (input("y: "))
+
+z = x + y
+
+print (x + y)
---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello, world!")
+
+x = int(input ("X: "))
+y = int(input ("Y: "))
+
+z = x+y
+
+# bellow are the 2 ways to slow output in python
+
+print (f"{z}")
+print (x + y)
